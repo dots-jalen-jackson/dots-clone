d17e7e7bc9a0a45594ee1b4b08c468201ef6fc6e
JayDiddyThaGOAT <jalenjacksoncs@gmail.com>
2021-09-15 10:19:23 -0700

    Added comments to Dots Color Pallete script

--- Assets/Scripts/Dots/DotsColorPalette.cs
+++ Assets/Scripts/Dots/DotsColorPalette.cs
@@ -2,15 +2,25 @@
 using UnityEngine;
 using Random = UnityEngine.Random;
 
+/// <summary>
+/// Stores the colors that will be used for the Dots in a color palette
+/// </summary>
 [System.Serializable]
 public struct DotsColorPalette
 {
-    public List<Color> Colors;
+    /// <summary>
+    /// List of colors that can be edited in the DotsBoard's inspector
+    /// </summary>
+    public List<Color> colors;
     
+    /// <summary>
+    /// Calculates random index between 0 and the number of colors in the color palette
+    /// </summary>
+    /// <returns>Random color in the color palette</returns>
     public Color GetRandomColor()
     {
-        int colorIndex = Random.Range(0, Colors.Count);
-        Color color = Colors[colorIndex];
+        int colorIndex = Random.Range(0, colors.Count);
+        Color color = colors[colorIndex];
         return color;
     }
 }
