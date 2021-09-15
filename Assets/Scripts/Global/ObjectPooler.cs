2b753348686f4c86d89658c68e4f189eeab5283d
JayDiddyThaGOAT <jalenjacksoncs@gmail.com>
2021-09-15 10:11:45 -0700

    Added comments & Removed isExpandable variable in ObjectPooler script

--- Assets/Scripts/Global/ObjectPooler.cs
+++ Assets/Scripts/Global/ObjectPooler.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class ObjectPooler : MonoBehaviour
 {
 #pragma warning disable 0649
+    /// <summary>
+    /// Stores the object that we are spawning in the pool
+    /// </summary>
     [SerializeField]
     private GameObject _objectPrefab;
+    
+    public GameObject ObjectPrefab => _objectPrefab;
 
-    [SerializeField]
-    private bool _isExpandable = false;
-
+    /// <summary>
+    /// Number of objects in the pool
+    /// </summary>
     private int _objectPoolSize;
 
     public int ObjectPoolSize
@@ -18,14 +23,19 @@ public class ObjectPooler : MonoBehaviour
         get => _objectPoolSize;
         set => _objectPoolSize = value;
     }
-
-    public GameObject ObjectPrefab => _objectPrefab;
     
 #pragma warning restore 0649
 
+    /// <summary>
+    /// Stores the objects in the pooler that aren't being used in the game
+    /// </summary>
     private List<GameObject> _freeList;
+    
+    /// <summary>
+    /// Stores the objects in the pooler that are being used in the game
+    /// </summary>
     private List<GameObject> _usedList;
-
+    
     void Awake()
     {
         _freeList = new List<GameObject>();
@@ -42,6 +52,9 @@ public class ObjectPooler : MonoBehaviour
         _freeList.Add(obj);
     }
     
+    /// <summary>
+    /// Generates multiple objects based on the number of objects in the pool
+    /// </summary>
     public void GenerateObjects()
     {
         for (int i = 0; i < ObjectPoolSize; i++)
@@ -56,16 +69,13 @@ public class ObjectPooler : MonoBehaviour
     public GameObject GetPooledObject()
     {
         if (_freeList.Count == 0)
-        {
-            if (!_isExpandable)
-                return null;
-            else
-                InstantiateNewObject();
-        }
-
+            return null;
+        
+        // Activate the last object in the pool
         GameObject obj = _freeList[_freeList.Count - 1];
         obj.SetActive(true);
         
+        // Move it to the used list & take it out of the free list
         _freeList.RemoveAt(_freeList.Count - 1);
         _usedList.Add(obj);
 
@@ -78,10 +88,13 @@ public class ObjectPooler : MonoBehaviour
     public void ReturnPooledObject(GameObject obj)
     {
         Debug.Assert(_usedList.Contains(obj));
+        
+        // Deactivate & reset the last object in the pool
         obj.SetActive(false);
         obj.transform.position = _objectPrefab.transform.position;
         obj.transform.rotation = _objectPrefab.transform.rotation;
-
+        
+        // Move it to the free list & take it out of the used list
         _usedList.Remove(obj);
         _freeList.Add(obj);
     }
